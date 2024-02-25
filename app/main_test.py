bdbf3ba2671ae4be3c546a9e94ee776d316b1b1c
Prasadchaskar <chaskarprasad2000@gmail.com>
2024-02-25 15:27:30 +0530

    update test case

--- app/main_test.py
+++ app/main_test.py
@@ -1,12 +1,20 @@
 from main import return_reverse_str
 import unittest
+from fastapi.testclient import TestClient
 import os
+from main import app
+import json
 
 class TestMain(unittest.TestCase):
+    def setUp(self):
+        self.client = TestClient(app)
+
     def test_return_reverse_str(self):
         random_str = "prasad"
-        random_str_reversed = "dasarp"
-        self.assertEqual(random_str_reversed, return_reverse_str(random_str))
+        expected_result = '"dasarp"'
+        response = self.client.get(f"/{random_str}")
+        print(response.text)  # Print response text to see its format
+        self.assertEqual(response.text, expected_result)
 
 if __name__ == "__main__":
     unittest.main()
