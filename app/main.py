52f4d2826eca4ee3402a0a854a879cb90d339a53
Prasadchaskar <chaskarprasad2000@gmail.com>
2024-02-25 14:51:45 +0530

    add comment

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hi Prasad"}
 
+# route to reverse string
 @app.get("/{random_string}")
 async def return_reverse_str(random_string:str):
     return "".join(reversed(random_string))
